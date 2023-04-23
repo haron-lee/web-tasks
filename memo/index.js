184424c172f4f3aee381f3309e7b069d22a4f2f2
haron-lee <haronxlee@gmail.com>
2023-04-23 23:56:23 +0900

    추가: 수정버튼 기능 구현

--- memo/index.js
+++ memo/index.js
@@ -1,22 +1,39 @@
 const saveBtn = document.getElementById("save_btn");
+const listTitle = document.getElementById("title");
+const listContent = document.getElementById("content");
 
 let allMemo = JSON.parse(localStorage.getItem("allMemo"));
 allMemo = allMemo ?? [];
 render();
 
-//* note를 loacalStorage에 저장
+//* note를 localStorage에 저장
 
 function saveNote() {
-  const listTitle = document.getElementById("title");
-  const listContent = document.getElementById("content");
   const title = document.getElementById("title").value;
   const content = document.getElementById("content").value;
-  allMemo.push({ title, content, len: allMemo.length });
-  localStorage.setItem("allMemo", JSON.stringify(allMemo));
-  // 저장하기 누르면 내부 value값은 없어지게
-  listTitle.value = "";
-  listContent.value = "";
+  const modifyIdx = saveBtn.getAttribute("data-idx");
 
+  // title과 content가 비어있다면 알림이 가게끔
+  if (title === "" || content === "") {
+    alert("제목 또는 내용을 작성해주세요");
+  }
+
+  // saveBtn이 저장하기여야하고, title과 content에 내용이 있어야 저장 실행
+  if (saveBtn.textContent === "저장하기" && !(!title || !content)) {
+    allMemo.push({ title, content, len: allMemo.length });
+    localStorage.setItem("allMemo", JSON.stringify(allMemo));
+    // 저장하기 누르면 내부 value값은 없어지게
+    listTitle.value = "";
+    listContent.value = "";
+  } else if (saveBtn.textContent === "수정하기" && modifyIdx !== null) {
+    allMemo[modifyIdx].title = title;
+    allMemo[modifyIdx].content = content;
+    localStorage.setItem("allMemo", JSON.stringify(allMemo));
+    listTitle.value = "";
+    listContent.value = "";
+    saveBtn.textContent = "저장하기";
+    saveBtn.removeAttribute("data-idx");
+  }
   render();
 }
 
@@ -25,21 +42,28 @@ function render() {
   noteDisplay.innerHTML = "";
 
   for (const item of allMemo) {
-    const saveBox = document.createElement("div");
+    const saveBox = document.createElement("li");
     const saveTitle = document.createElement("h2");
     const saveContent = document.createElement("p");
     const saveId = document.createElement("p");
+    const btnBox = document.createElement("div");
+    const modifyBtn = document.createElement("button");
     const deleteMemoBtn = document.createElement("button");
 
     saveBox.classList.add("list");
     saveId.classList.add("list-id");
     saveTitle.classList.add("list-title");
     saveContent.classList.add("list-content");
+    btnBox.classList.add("box-btn");
+    modifyBtn.classList.add("list-modify");
     deleteMemoBtn.classList.add("list-delete");
 
     saveTitle.textContent = item.title;
     saveContent.textContent = item.content;
     saveId.textContent = item.len + 1;
+    modifyBtn.textContent = "수정";
+    modifyBtn.setAttribute("id", item.len);
+    modifyBtn.setAttribute("onclick", "modify()");
     deleteMemoBtn.textContent = "삭제";
     deleteMemoBtn.setAttribute("id", item.len);
     deleteMemoBtn.setAttribute("onclick", "remove()");
@@ -47,10 +71,23 @@ function render() {
     saveBox.appendChild(saveId);
     saveBox.appendChild(saveTitle);
     saveBox.appendChild(saveContent);
-    saveBox.appendChild(deleteMemoBtn);
+    btnBox.appendChild(modifyBtn);
+    btnBox.appendChild(deleteMemoBtn);
+    saveBox.appendChild(btnBox);
     noteDisplay.appendChild(saveBox);
   }
 }
+
+const modify = () => {
+  const idx = allMemo.findIndex((item) => item.len == event.srcElement.id);
+  if (idx !== -1) {
+    listTitle.value = allMemo[idx].title;
+    listContent.value = allMemo[idx].content;
+    saveBtn.textContent = "수정하기";
+    // 수정 버튼의 id를 변경하여 수정할 항목을 식별하도록 함
+    saveBtn.setAttribute("data-idx", idx);
+  }
+};
 
 function remove() {
   const idx = allMemo.find((item) => item.len == event.srcElement.id);
@@ -65,8 +102,23 @@ function remove() {
   render();
 }
 
-// new note누르면은 note안의 내용이 clear되기
-
-// 목록중의 하나 누르면은 모달처럼 펼쳐지기
+document.querySelector("#newNote").addEventListener("click", () => {
+  if (listTitle.value || listContent.value) {
+    listTitle.value = "";
+    listContent.value = "";
+  }
+});
 
 saveBtn.addEventListener("click", saveNote);
+
+// TODo
+// new note누르면은 note안의 내용이 clear되기 V
+// 수정하기 기능 V
+
+//* 날짜도 넣어볼까?
+// 2023년 4월 23일 09:25 이런식으로?
+// 2023/4/23 09:25 이런식으로?
+
+//* 목록중의 하나 누르면은 모달처럼 펼치기.
+// 목록이랑, 작성부분 위로 떠오르게 하면 좋을 것 같은디..
+// 그 안에 수정,삭제버튼도 들어가고 수정버튼 누르면 현재로서는 모달창이 꺼지면서 텍스트 작성하는 곳으로 불러오는 수 밖에 없을듯
