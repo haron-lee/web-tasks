f1aa32679a03f4c3334615038ff69635f1ead3d1
haron-lee <haronxlee@gmail.com>
2023-05-04 02:48:43 +0900

    [Fix] 연산자 중복처리, 점 중복처리, 점 누르면 0함께 입력

--- calculator/index.js
+++ calculator/index.js
@@ -9,47 +9,53 @@ function clearDisplay() {
   display.innerText = '0';
 }
 
-// display.textContent에 저장된 수식을 문자열로 받아들이고, 이를 new Function() 생성자에 전달하여 새로운 함수를 생성. 이 함수를 실행하여 결과값을 계산하고, 계산된 결과값을 다시 display.textContent에 저장.
 //* 계산하는 역할
 function evaluateExpression() {
-  const expression = display.textContent; // 7+7
-  const result = new Function(`return ${expression}`)(); // eval
+  const expression = display.textContent;
+  const result = new Function(`return ${expression}`)();
   display.textContent = result;
 }
 
 function handleClick(event) {
   const target = event.target;
-  const buttonValue = target.textContent;
-  const displayValue = display.textContent;
-  const last = displayValue.charAt(displayValue.length - 1);
 
-  if (target.matches('.num')) {
-    // num일 경우
-    if (displayValue === '0') {
-      // display 텍스트가 0이면 button의 숫자를 입력
-      display.textContent = buttonValue;
-    } else {
-      // 그게 아니면 display의 text와 button의 텍스트를 이어붙이기
-      display.textContent = displayValue + buttonValue;
-    }
-  } else if (target.matches('.operator')) {
-    if (displayValue === '0') {
-      return;
-    }
-    display.textContent = displayValue + ' ' + buttonValue + ' ';
-  } else if (target === clearButton) {
+  if (target === clearButton) {
     clearDisplay();
   } else if (target === enterButton) {
     evaluateExpression();
   }
 }
 
-numbers.forEach((button) => {
-  button.addEventListener('click', handleClick);
+numbers.forEach((num) => {
+  num.addEventListener('click', (event) => {
+    const displayValue = display.textContent;
+    const target = event.target;
+    const last = display.textContent.charAt(display.textContent.length - 1);
+
+    if (target.textContent === '.' && displayValue.includes('.')) {
+      // 이미 소수점이 있는 경우
+      return; // 더 이상 추가하지 않음
+    } else if (target.textContent === '.' && displayValue === '0') {
+      display.textContent = '0' + target.textContent;
+    } else if (displayValue === '0') {
+      display.textContent = target.textContent;
+    } else {
+      display.textContent = display.textContent + target.textContent;
+    }
+  });
 });
 
-operators.forEach((button) => {
-  button.addEventListener('click', handleClick);
+operators.forEach((operator) => {
+  operator.addEventListener('click', (event) => {
+    const target = event.target;
+    const displayValue = display.textContent;
+    const last = display.textContent.charAt(display.textContent.length - 1);
+
+    if (operatorsArr.includes(last)) {
+      return;
+    }
+    display.textContent = displayValue + target.textContent;
+  });
 });
 
 clearButton.addEventListener('click', handleClick);
@@ -58,10 +64,9 @@ enterButton.addEventListener('click', handleClick);
 // TODO
 // 숫자 빠져나가는 거 제한하기
 
-// 연산자 중복처리.
-// '.' 버튼과 연산자 버튼이 중복 입력되는 부분 수정
-//  숫자 -> 연산자 -> 0 중복처리.
+// 연산자 중복처리. v
+// '.' 버튼과 연산자 버튼이 중복 입력되는 부분 수정 v
 // 연산 중에는 숫자 제일 앞에 0이 입력되는 부분
 
 // 천단위 콤마처리.
-// '0'인 상태에서 '.'을 눌렀을 때 '.'으로만 표시되는 부분을 '0.'으로 표시되도록 수정 (. 따로 처리)
+// '0'인 상태에서 '.'을 눌렀을 때 '.'으로만 표시되는 부분을 '0.'으로 표시되도록 수정 (. 따로 처리)  v
